f0cb7c3198cf2950b67bdab4ca11573495658dcc
akshit-mehra <akshitme03@gmail.com>
2023-06-13 20:54:07 +0530

    initial commit- message: adding mongoose schema

---
+++ backend/models/Products.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description : { type: String, required: true},
+  price: { type: Number, required: true },
+  availability: { type: Boolean, required: true, default: true },
+  seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
---
+++ backend/models/Users.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true},
+  password: { type: String, required: true },
+  college: { type: String, required: true},
+  date: { type: Date, default: Date.now }
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
