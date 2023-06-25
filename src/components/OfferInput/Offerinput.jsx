038b32331934d0436a3233056f981f3526dbb803
akshit-mehra <akshitme03@gmail.com>
2023-06-25 20:20:10 +0530

    adding option to donate items

--- src/components/OfferInput/Offerinput.jsx
+++ src/components/OfferInput/Offerinput.jsx
@@ -1,42 +1,55 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import appContext from "../../context/AppContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Offerinput = () => {
-    const context = useContext(appContext);
-  const {makeoffer, offerProps }=context;
+  const context = useContext(appContext);
+  const { makeoffer, offerProps } = context;
   console.log(offerProps);
   // const data = useLocation().state.request;
 
   const handleChange = (e) => {
     setoffVal({ ...offVal, [e.target.name]: e.target.value });
-    console.log(offVal);
+    // console.log(offVal);
+    console.log(e.target.name + " " + e.target.value);
   };
-  
 
+  const [donateItem, setdonateItem] = useState(false);
 
   const [offVal, setoffVal] = useState({
     offerTitle: "",
     offerDescription: "",
     offerAmount: "",
     offerCondition: "Good",
-    offerLocation:"",
+    offerLocation: "",
   });
+
+  const donationChange = async (e) => {
+    
+    setdonateItem(!donateItem);
+
+    setTimeout(() => {
+      console.log(donateItem);  
+    }, 100);
+  
+  }
 
 
   const navigate = useNavigate();
-  const submitAd = async(e) => {
+  const submitAd = async (e) => {
     e.preventDefault();
-  
+
+    let finalOfferAmount = donateItem ? 0 : offVal.offerAmount;
+
     await makeoffer(
-        offerProps.id,
-        offVal.offerTitle,
-        offVal.offerAmount,
-        offVal.offerDescription,
-        offVal.offerCondition,
-        offVal.offerLocation
+      offerProps.id,
+      offVal.offerTitle,
+      finalOfferAmount,
+      offVal.offerDescription,
+      offVal.offerCondition,
+      offVal.offerLocation
     )
     navigate("/request");
 
@@ -76,7 +89,7 @@ const Offerinput = () => {
       >
         <p>Enter Details so the user can make a decision</p>
       </div>
-      <div className="d-flex aligns-items-center" style={{marginLeft:'50px',marginTop:'10px'}}>
+      <div className="d-flex aligns-items-center" style={{ marginLeft: '50px', marginTop: '10px' }}>
         <form className="row g-3 ">
           {/* first row of input form */}
           <div className="col-md-6">
@@ -102,12 +115,17 @@ const Offerinput = () => {
                 placeholder="Price"
                 name="offerAmount"
                 onChange={handleChange}
+                // disabled={donateItem}
+                value={donateItem ? 0 : offVal.offerAmount}
+                disabled={donateItem}
               />
               <label htmlFor="offerAmount">Enter Price(in ₹)</label>
             </div>
           </div>
 
-          <div className="col-md-3"></div>
+          <div className="col-md-3">
+
+          </div>
 
           <div className="col-md-4">
             <div className="form-floating">
@@ -128,9 +146,15 @@ const Offerinput = () => {
               <label htmlFor="offerCondition">Condition of Product</label>
             </div>
           </div>
-          
+          <div className="col-md-2 d-flex justify-content-center align-items-center">
+          <div className="form-check">
+  <input className="form-check-input" onChange={donationChange} type="checkbox" value="" id="defaultCheck1" />
+  <label className="form-check-label" htmlFor="defaultCheck1">
+    Donate Item
+  </label>
+</div>
+          </div>
 
-          
 
           <div className="mb-3 col-md-9">
             <textarea
