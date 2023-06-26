ecce9de49b607aab4210af44026d2597e4978c20
aadhar2003 <68272092+aadhar2003@users.noreply.github.com>
2023-06-26 15:25:32 +0530

    complete

--- src/components/PlaceCards/PlaceCards.jsx
+++ src/components/PlaceCards/PlaceCards.jsx
@@ -3,13 +3,11 @@ import "./PlaceCards.css";
 import appContext from "../../context/AppContext";
 import Cards from "../Cards/Cards";
 
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-
 const PlaceCards = () => {
-
   const context = useContext(appContext);
   const { listing, getAllList } = context;
 
@@ -17,33 +15,45 @@ const PlaceCards = () => {
     getAllList();
   }, []);
 
-
   const [search, setsearch] = useState("");
   const [searchCat, setsearchCat] = useState("");
 
   const handlechange = (e) => {
     setsearch(e.target.value);
     console.log(search);
-  }
-  
+  };
+
   const changeCategory = (e) => {
     setsearchCat(e.target.value);
-  }
+  };
 
   return (
     <div>
-      <div className='searchBar'>
-        <FontAwesomeIcon icon={faSearch} id='s-icon' />
-        <input type="text" name="search" value={search} id="" placeholder='Search an Item' onChange={handlechange} />
+      <div className="searchBar">
+        <FontAwesomeIcon icon={faSearch} id="s-icon" />
+        <input
+          type="text"
+          name="search"
+          value={search}
+          id=""
+          placeholder="Search an Item"
+          onChange={handlechange}
+        />
         <div className="col-md-1" id="cat-selector">
-
-          <select class="form-select" id="change-drop" aria-label="Default select example" onChange={changeCategory}>
-            <option value="Categories" selected>Categories</option>
+          <select
+            class="form-select"
+            id="change-drop"
+            aria-label="Default select example"
+            onChange={changeCategory}
+          >
+            <option value="Categories" selected>
+              Categories
+            </option>
             <option value="Books">Books</option>
             <option value="Electronics">Electronics</option>
             <option value="Automobiles">Automobiles</option>
             <option value="Appliances">Appliances</option>
-            <option value="SportsEquipment" >Sports Eqipment</option>
+            <option value="SportsEquipment">Sports Eqipment</option>
             <option value="Games">Games</option>
             <option value="Furniture">Furniture</option>
             <option value="Other">Other</option>
@@ -55,22 +65,28 @@ const PlaceCards = () => {
       <div className="pl-h d-flex justify-content-around">
         {listing.map((res) => {
           {
-            if ((res.title.toUpperCase().indexOf(search.toUpperCase()) > -1) && (searchCat == "" || searchCat == "Categories" || res.category==searchCat)) {
-              return (
-                <div className="c-data">
-                  <Cards
-                    id={res._id}
-                    key={res._id}
-                    imageURL={res.imageURL}
-                    category={res.category}
-                    title={res.title}
-                    price={res.price}
-                  />
-                </div>
-              );
+            if (
+              res.title.toUpperCase().indexOf(search.toUpperCase()) > -1 &&
+              (searchCat == "" ||
+                searchCat == "Categories" ||
+                res.category == searchCat)
+            ) {
+              if (res.availability == true) {
+                return (
+                  <div className="c-data">
+                    <Cards
+                      id={res._id}
+                      key={res._id}
+                      imageURL={res.imageURL}
+                      category={res.category}
+                      title={res.title}
+                      price={res.price}
+                    />
+                  </div>
+                );
+              }
             }
           }
-
         })}
       </div>
     </div>
--- src/components/Profile/Customers.jsx
+++ src/components/Profile/Customers.jsx
@@ -12,14 +12,23 @@ const Customers = () => {
 
   const context = useContext(appContext);
 
- const { potentialCustomers,getbuyers,profileListId ,getAllUsers,users, setchatProductId, setchatConvId} = context;
- 
- useLayoutEffect(() => {
-  getbuyers(profileListId);
-  getAllUsers();
-  console.log(users);
-  console.log(potentialCustomers);
- },[])
+  const {
+    potentialCustomers,
+    getbuyers,
+    profileListId,
+    getAllUsers,
+    users,
+    setchatProductId,
+    setchatConvId,
+    marklisting
+  } = context;
+
+  useLayoutEffect(() => {
+    getbuyers(profileListId);
+    getAllUsers();
+    console.log(users);
+    console.log(potentialCustomers);
+  }, []);
 
   const OpenChat = (e) => {
     // console.log(e.target.value);
@@ -28,28 +37,70 @@ const Customers = () => {
     setchatProductId(profileListId);
 
     navigate("/chat");
+  };
+  const handleDeal=()=>{
+    marklisting(potentialCustomers[0].Productid);
   }
 
   return (
-    <div className="d-flex justify-content-around mt-5" style={{flexWrap:'wrap'}}>
-      {
-        potentialCustomers.map((item) => {
-          return users.map((user) => {
-            if (item.offeredBy === user._id) {
-              return (
-                <div  style={{ width:'300px',marginRight:'10px',border:'1px solid #00000042',fontSize:'25px',textAlign:'center',display:'flex',flexDirection:'column',justifyContent:'center',flexBasis:'30%',margin:'10px 0px'}}>
-                  <p1 style={{padding:'7px',whiteSpace:'nowrap'}}> Customer name:{user.name}</p1>
-                  <button style={{width:'60%',alignSelf:'center',padding:'3px',minHeight:'fit-content',minWidth:'fit-content'}} id="signIn" value={user._id} onClick={OpenChat}>Chat with customer</button>
-
+    <div
+      className="d-flex justify-content-around mt-5"
+      style={{ flexWrap: "wrap" }}
+    >
+      {potentialCustomers.map((item) => {
+        return users.map((user) => {
+          if (item.offeredBy === user._id) {
+            return (
+              <div
+                style={{
+                  width: "300px",
+                  marginRight: "10px",
+                  border: "1px solid #00000042",
+                  fontSize: "25px",
+                  textAlign: "center",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  flexBasis: "30%",
+                  margin: "10px 0px",
+                }}
+              >
+                <p1 style={{ padding: "7px", whiteSpace: "nowrap" }}>
+                  {" "}
+                  Customer name:{user.name}
+                </p1>
+                <div className="d-flex justify-content-between">
+                  <button
+                    style={{
+                      width: "50%",
+                      alignSelf: "center",
+                      padding: "",
+                      minHeight: "fit-content",
+                      minWidth: "fit-content",
+                    }}
+                    id="signIn"
+                    value={user._id}
+                    onClick={OpenChat}
+                  >
+                    Chat with customer
+                  </button>
+                  <button id="signIn"  style={{
+                      width: "50%",
+                      alignSelf: "center",
+                      padding: "",
+                      minHeight: "fit-content",
+                      minWidth: "fit-content",
+                      marginRight:'0'
+                    }}
+                    onClick={handleDeal}>
+                    Done Deal
+                  </button>
                 </div>
-              );
-            }
-          })
-            
-          
-        })
-      }
-
+              </div>
+            );
+          }
+        });
+      })}
     </div>
   );
 };
